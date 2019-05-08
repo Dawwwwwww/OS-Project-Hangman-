c6b09f21f146fa2e288d135ac9d3a11375548434
misterbankth <50046691+misterbankth@users.noreply.github.com>
2019-05-08 14:38:40 +0700

    Update and rename Hangman.java to Server.java

---
+++ Server.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.lang.ClassNotFoundException;
+import java.net.Socket;
+import java.util.Random;
+
+public class Server {
+    //socket server port on which it will listen
+    private static int PORT = 8888;
+    //static ServerSocket variable
+    private static ServerSocket server;
+
+    public String OUTPUT = "PRINT";
+    public String INPUT = "INPUT";
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        int i =0;
+        Thread thread = null;
+        try {
+            //Create ServerSocket
+            server = new ServerSocket(PORT);
+            while (true) {
+                System.out.println("Waiting for the client request");
+                Socket socket = server.accept();
+                ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                String message = (String) ois.readObject();
+                Random rand = new Random();
+                //Generate Thread and port
+                int newPort = rand.nextInt(9000)+1000;
+                ServerProtocal serverProtocal = new ServerProtocal(newPort);
+                thread = new Thread(serverProtocal);
+                thread.start();
+                ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                oos.writeObject(""+newPort);
+
+                ois.close();
+                oos.close();
+                socket.close();
+                i++;
+            }
+        }
+        catch (IOException ex) {
+            try {
+                server.close();
+            } catch (IOException e) {
+                System.err.println("ERROR closing socket: " + e.getMessage());
+            }
+        }
+    }
+}
